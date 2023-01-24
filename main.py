a2883e0eadede2c48e153b0ccb7c5ec5f20d19b1
EGGEN Morten <morten.eggen@soprasteria.com>
2023-01-25 00:21:12 +0100

    make new or use old workbook

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from xlutils.copy import copy
 import xlrd
 
 
-url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.91601&lon=10.85363'
+url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.63333&lon=11.11667'
 
 headers = {
     'User-Agent': 'My Agent 1.0',
@@ -21,16 +21,22 @@ data = response.json()
 print(data["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
 
 
+try:
+    rb = xlrd.open_workbook("G:\My Drive\Personlige Prosjekt\Været\Weather.xls")
+    r_sheet = rb.sheet_by_index(0)
+    wb = copy(rb)
+    w_sheet = wb.get_sheet(0)
+except:
+    wb = xlwt.Workbook()
+    w_sheet = wb.add_sheet('sheet1')
 
-rb = xlrd.open_workbook("G:\My Drive\Personlige Prosjekt\Været\Weather.xls")
-r_sheet = rb.sheet_by_index(0)
-wb = copy(rb)
-w_sheet = wb.get_sheet(0)
-
-w_sheet.write(0,0,"Temperatur:")
-
+style1 = xlwt.XFStyle()
+style1.num_format_str = 'DD-MM-YY'
 cell_row = datetime.now().timetuple().tm_yday
+today = date.today()
+print(today)
+w_sheet.write(cell_row-23, 0, today, style1)
+w_sheet.write(cell_row-23, 1, data["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
 
-w_sheet.write(cell_row-24, 1, data["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
 
 wb.save("G:\My Drive\Personlige Prosjekt\Været\Weather.xls")
