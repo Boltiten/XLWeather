ccbcb38cd64b7403f38698200c55b76a866ffb95
EGGEN Morten <morten.eggen@soprasteria.com>
2023-01-24 19:33:55 +0100

    write to existing file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 import requests
+from datetime import datetime, date
+import xlwt
+from xlutils.copy import copy
+import xlrd
+
 
 url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.91601&lon=10.85363'
 
@@ -16,11 +21,16 @@ data = response.json()
 print(data["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
 
 
-import xlwt
-wb = xlwt.Workbook()
 
-sheet1 = wb.add_sheet('Sheet 1')
-sheet1.write(0,0,"Temperatur:")
-sheet1.write(0,1, data["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
+rb = xlrd.open_workbook("G:\My Drive\Personlige Prosjekt\Været\Weather.xls")
+r_sheet = rb.sheet_by_index(0)
+wb = copy(rb)
+w_sheet = wb.get_sheet(0)
 
-wb.save("Weather.xls")
+w_sheet.write(0,0,"Temperatur:")
+
+cell_row = datetime.now().timetuple().tm_yday
+
+w_sheet.write(cell_row-24, 1, data["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
+
+wb.save("G:\My Drive\Personlige Prosjekt\Været\Weather.xls")
